c89e203bd9b02ea97e48df0c1f626cd39c934255
vanrajboricha <86003110+vanrajboricha@users.noreply.github.com>
2026-06-15 18:53:09 +0530

    Delete sqs_queue/vanbor-SQS-prod directory

--- sqs_queue/vanbor-SQS-prod/outputs.tf
+++
@@ -1,9 +0,0 @@
-
-output "queue_url" {
-  value       = aws_sqs_queue.main.url
-  description = "SQS queue URL"
-}
-output "queue_arn" {
-  value       = aws_sqs_queue.main.arn
-  description = "SQS queue ARN"
-}
